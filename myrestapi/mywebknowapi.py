57ead079a90012ad29f6a748a3fa35870e7db6b7
test <test@test.com>
2017-02-05 13:36:13 +1100

    Completed API implementation

--- myrestapi/mywebknowapi.py
+++ myrestapi/mywebknowapi.py
@@ -22,6 +22,19 @@ def welcomepage():
     mymsg = [{'message': 'Welcome to mywebknow REST API'}]
     return jsonify(mymsg)
 
+#If not value passed return error
+@app.route('/find/', methods=['GET'])
+@app.route('/findauthor/', methods=['GET'])
+@app.route('/findlabel/', methods=['GET'])
+@app.route('/finddatetime/', methods=['GET'])
+@app.route('/findheadline/', methods=['GET'])
+@app.route('/findurl/', methods=['GET'])
+@app.route('/findstandfirstinfo/', methods=['GET'])
+def errorparam():
+    mymsg = [{'error': 'No parameter passed. Please refer to API documentation'}]
+    app.logger.error(mymsg)
+    return jsonify(mymsg)  # Because API user can parse it
+
 #To fetch all data from mongodb
 @app.route('/all', methods=['GET'])
 def alldata():
@@ -52,7 +65,7 @@ def alldata():
 
 """
 This method will facilitate the search of all data with existence of any keyword
-Keyworkd will be passed as value
+Keyword will be passed as value
 """
 @app.route('/find/<value>', methods=['GET'])
 def finddata(value):
@@ -71,12 +84,210 @@ def finddata(value):
         #Fetch the abody data that satisfies the regular expression
         docs = collection.find({'abody': regx}, {'_id': False})
 
-        #Create a list of all data
+        #Create a list of found articles
         for doc in docs:
             items.append(doc)
 
     except Exception as e:
         mymsg = [{'error': 'Problem fetching data'},{'exception': e.message}]
+        app.logger.error(mymsg)
+        return jsonify(mymsg)  # Because API user can parse it
+
+    #Return data in json format to caller
+    return jsonify(items)
+
+"""
+This method will facilitate the search of all authors articles with existence of keyword
+Keyword will be passed as value
+"""
+@app.route('/findauthor/<author>', methods=['GET'])
+def findauthor(author):
+    items = []
+
+    try:
+        uri = getdbclienturi()
+        # Client to mongodb database
+        client = pymongo.MongoClient(uri)
+        # Selecting the database
+        db = client['mywebknow']
+        # Selecting the collection
+        collection = db['articles']
+        #Generate the regular expression to fetch articles written  by any author  with name containing the value anywhere
+        regx = re.compile("[\w]*" + author + "[\w]*", re.IGNORECASE)
+        #Fetch the author article that satisfies the regular expression
+        docs = collection.find({'author': regx}, {'_id': False})
+
+        #Create a list of found articles
+        for doc in docs:
+            items.append(doc)
+
+    except Exception as e:
+        mymsg = [{'error': 'Problem fetching author'},{'exception': e.message}]
+        app.logger.error(mymsg)
+        return jsonify(mymsg)  # Because API user can parse it
+
+    #Return data in json format to caller
+    return jsonify(items)
+
+"""
+This method will facilitate the search of all labels with existence of keyword
+Keyword will be passed as label
+"""
+@app.route('/findlabel/<label>', methods=['GET'])
+def findlabel(label):
+    items = []
+
+    try:
+        uri = getdbclienturi()
+        # Client to mongodb database
+        client = pymongo.MongoClient(uri)
+        # Selecting the database
+        db = client['mywebknow']
+        # Selecting the collection
+        collection = db['articles']
+        #Generate the regular expression to fetch any arcticle with label containing the value
+        regx = re.compile("[\w]*" + label + "[\w]*", re.IGNORECASE)
+        #Fetch the label that satisfies the regular expression
+        docs = collection.find({'albl': regx}, {'_id': False})
+
+        #Create a list of found articles
+        for doc in docs:
+            items.append(doc)
+
+    except Exception as e:
+        mymsg = [{'error': 'Problem fetching articles with label'},{'exception': e.message}]
+        app.logger.error(mymsg)
+        return jsonify(mymsg)  # Because API user can parse it
+
+    #Return data in json format to caller
+    return jsonify(items)
+
+"""
+This method will facilitate the search of all articles containing date time part passed
+Keyword will be passed as datetimepart
+"""
+@app.route('/finddatetime/<datetimepart>', methods=['GET'])
+def finddatetime(datetimepart):
+    items = []
+
+    try:
+        uri = getdbclienturi()
+        # Client to mongodb database
+        client = pymongo.MongoClient(uri)
+        # Selecting the database
+        db = client['mywebknow']
+        # Selecting the collection
+        collection = db['articles']
+        #Generate the regular expression to fetch any arcticle with given date time part
+        regx = re.compile("[\w]*" + datetimepart + "[\w]*", re.IGNORECASE)
+        #Fetch the articles that satisfies the regular expression
+        docs = collection.find({'adt': regx}, {'_id': False})
+
+        #Create a list of found articles
+        for doc in docs:
+            items.append(doc)
+
+    except Exception as e:
+        mymsg = [{'error': 'Problem fetching article with date time part'},{'exception': e.message}]
+        app.logger.error(mymsg)
+        return jsonify(mymsg)  # Because API user can parse it
+
+    #Return data in json format to caller
+    return jsonify(items)
+
+"""
+This method will facilitate the search of all articles containing parameter in headline
+Keyword will be passed as headline
+"""
+@app.route('/findheadline/<headline>', methods=['GET'])
+def findheadline(headline):
+    items = []
+
+    try:
+        uri = getdbclienturi()
+        # Client to mongodb database
+        client = pymongo.MongoClient(uri)
+        # Selecting the database
+        db = client['mywebknow']
+        # Selecting the collection
+        collection = db['articles']
+        #Generate the regular expression to fetch any arcticle with given value in headline
+        regx = re.compile("[\w]*" + headline + "[\w]*", re.IGNORECASE)
+        #Fetch the articles that satisfies the regular expression
+        docs = collection.find({'ahead': regx}, {'_id': False})
+
+        #Create a list of found articles
+        for doc in docs:
+            items.append(doc)
+
+    except Exception as e:
+        mymsg = [{'error': 'Problem fetching article with headline part'},{'exception': e.message}]
+        app.logger.error(mymsg)
+        return jsonify(mymsg)  # Because API user can parse it
+
+    #Return data in json format to caller
+    return jsonify(items)
+
+"""
+This method will facilitate the search of all articles containing parameter in url
+Keyword will be passed as url
+"""
+@app.route('/findurl/<url>', methods=['GET'])
+def findurl(url):
+    items = []
+
+    try:
+        uri = getdbclienturi()
+        # Client to mongodb database
+        client = pymongo.MongoClient(uri)
+        # Selecting the database
+        db = client['mywebknow']
+        # Selecting the collection
+        collection = db['articles']
+        #Generate the regular expression to fetch any arcticle with given value in headline
+        regx = re.compile("[\w]*" + url + "[\w]*", re.IGNORECASE)
+        #Fetch the articles that satisfies the regular expression
+        docs = collection.find({'url': regx}, {'_id': False})
+
+        #Create a list of found articles
+        for doc in docs:
+            items.append(doc)
+
+    except Exception as e:
+        mymsg = [{'error': 'Problem fetching article with url part'},{'exception': e.message}]
+        app.logger.error(mymsg)
+        return jsonify(mymsg)  # Because API user can parse it
+
+    #Return data in json format to caller
+    return jsonify(items)
+
+"""
+This method will facilitate the search of all articles containing parameter in stand first information
+Keyword will be passed as url
+"""
+@app.route('/findstandfirstinfo/<sfi>', methods=['GET'])
+def findstandfirstinfo(sfi):
+    items = []
+
+    try:
+        uri = getdbclienturi()
+        # Client to mongodb database
+        client = pymongo.MongoClient(uri)
+        # Selecting the database
+        db = client['mywebknow']
+        # Selecting the collection
+        collection = db['articles']
+        #Generate the regular expression to fetch any arcticle with given value in stand first information
+        regx = re.compile("[\w]*" + sfi + "[\w]*", re.IGNORECASE)
+        #Fetch the articles that satisfies the regular expression
+        docs = collection.find({'asfi': regx}, {'_id': False})
+
+        #Create a list of found articles
+        for doc in docs:
+            items.append(doc)
+
+    except Exception as e:
+        mymsg = [{'error': 'Problem fetching article with stand first information part'},{'exception': e.message}]
         app.logger.error(mymsg)
         return jsonify(mymsg)  # Because API user can parse it
 
